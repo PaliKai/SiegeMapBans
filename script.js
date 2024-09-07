2cf98810f676f610d502eb68ff1fd03005ec4cf6
PaliKai <67027227+PaliKai@users.noreply.github.com>
2024-09-07 15:55:49 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,179 +1,114 @@
-attackers = null
-defenders = null
-
-var currentlyDefending = false
-
-var label = document.getElementById("Label")
-var portrait = document.getElementById("Portrait")
-var icon = document.getElementById("Icon")
-
-var label2 = document.getElementById("Label2")
-var portrait2 = document.getElementById("Portrait2")
-var icon2 = document.getElementById("Icon2")
-
-var divider = document.getElementById("BlueSide")
-
-var scroll = 0
-
-var Op1 = "", Op2 = ""
-
-fetch('./ops.json')
+var mainJson
+fetch('./maps.json')
     .then((response) => response.json())
-    .then(json => loadOps(json));
+    .then(json => loadMaps(json));
 
-function toggleTeam() {
-    var audio = new Audio("Sounds/SwitchTeams.wav");
+ranked = null
+other = null
+
+
+var mapsDiv = document.getElementById("maps")
+
+var resetButton = document.getElementById("reset")
+
+function loadMaps(json) {
+    mainJson = json;
+
+    ranked = json.ranked
+    other = json.other
+
+    for (let map of ranked) {
+        addMap(map)
+    }
+}
+
+function addMap(map) {
+    let mapElement = new MapElement(map)
+    mapsDiv.appendChild(mapElement.mainDiv)
+}
+
+function clearMaps() {
+    while (mapsDiv.firstElementChild) {
+        mapsDiv.removeChild(mapsDiv.firstElementChild)
+    }
+}
+
+function random5() {
+    clearMaps();
+
+    resetButton.style.display = "block"
+
+    let unchosenMaps = [...ranked]
+
+    for (let i = 0; i < 5; i++) {
+        let chosenMap = unchosenMaps.splice(Math.floor(Math.random()*unchosenMaps.length), 1)[0];
+
+        addMap(chosenMap);
+    }
+
+    var audio = new Audio("Sounds/BanMap.wav");
     audio.volume = 0.2;
-    audio.play();
-    currentlyDefending = divider.classList.toggle("defending")
-
-    scroll = 0
-    scrollVel = 0
-    setScroll(0)
-    document.getElementById("Operator").style.setProperty("--Mod", 0)
-    loadDefault()
+    audio.play()
 }
 
+function reset() {
+    resetButton.style.display = "none"
 
-function loadOps(json) {
-    attackers = json.attackers
-    defenders = json.defenders
+    clearMaps()
 
-    loadDefault()
+    loadMaps(mainJson)
+
+    var audio = new Audio("Sounds/BanMap.wav");
+    audio.volume = 0.2;
+    audio.play()
 }
 
-function loadDefault() {
-    let list = attackers;
-    if (currentlyDefending) {
-        list = defenders;
-    }
-    operator = list[0]
-    // console.log(operator.name)
-    loadOperator1(operator)
+class MapElement {
+    /** @type {HTMLElement} */
+    mainDiv = null;
+    /** @type {HTMLElement} */
+    innerDiv = null;
+    /** @type {HTMLElement} */
+    pictureDiv = null;
+    /** @type {HTMLElement} */
+    overlayDiv = null;
+    /** @type {HTMLElement} */
+    labelH1 = null;
 
-    operator = list[1]
-    loadOperator2(operator)
-}
+    constructor(map) {
 
-function randomOp() {
-    let list = attackers;
-    if (currentlyDefending) {
-        list = defenders;
-    }
-    opIndex = Math.floor(Math.random()*list.length)
-    return list[opIndex]
-}
+        this.mainDiv = document.createElement("div");
+        this.mainDiv.classList.add("map");
 
-function loadOperator1(op) {
-    // console.log(op.name)
-    
-    label.innerText = op.name.toUpperCase()
-    portrait.style.backgroundImage = `url('${op.picture}')`
-    icon.style.backgroundImage = `url('${op.icon}')`
+        this.mainDiv.onclick = () => toggleBan(this);
 
-    Op1 = op.name;
+        this.innerDiv = document.createElement("div");
+        this.innerDiv.classList.add("inner");
 
-}
+        this.mainDiv.appendChild(this.innerDiv)
 
-function loadOperator2(op) {
-    // console.log(op.name)
-    
-    label2.innerText = op.name.toUpperCase()
-    portrait2.style.backgroundImage = `url('${op.picture}')`
-    icon2.style.backgroundImage = `url('${op.icon}')`
+        this.pictureDiv = document.createElement("div");
+        this.pictureDiv.classList.add("picture");
+        this.pictureDiv.style.backgroundImage = `url('${map.picture}')`;
 
-    Op2 = op.name;
-}
+        this.overlayDiv = document.createElement("div");
+        this.overlayDiv.classList.add("overlay");
+        this.pictureDiv.appendChild(this.overlayDiv);
 
-function getScroll() {
-    return parseInt(getComputedStyle(document.getElementById("Frame")).getPropertyValue("--Offset").replace("px", ""))
-}
+        this.innerDiv.appendChild(this.pictureDiv);
 
-function setScroll(scroll) {
-    document.getElementById("Frame").style.setProperty("--Offset", scroll + "px")
-}
+        this.labelH1 = document.createElement("h1");
+        this.labelH1.classList.add("label");
+        this.labelH1.innerText = map.name.toUpperCase();
 
-var scrollVel = 0
-const scrollDeccel = 2;
+        this.innerDiv.appendChild(this.labelH1);
 
-function roll() {
-    if (currentAudio != null) {
-        currentAudio.pause()
-        currentAudio = null;
-    }
-    if (scrollVel == 0) {
-        scrollVel = 300+Math.random()*200
-
-        window.requestAnimationFrame(rolling);
     }
 }
 
-function rolling() {
-    if (scrollVel > Math.abs(300-scroll%300)/10) {
-        if (scroll >= -300 && scroll-scrollVel/10 < -300) {
-            loadOperator1(randomOp())
-            var audio = new Audio("Sounds/click.wav");
-            audio.volume = 0.2;
-            audio.play()
-        }
-
-        if (scroll >= -600 && scroll-scrollVel/10 < -600) {
-            loadOperator2(randomOp())
-            var audio = new Audio("Sounds/click.wav");
-            audio.volume = 0.2;
-            audio.play()
-        }
-
-        scroll-=scrollVel/10
-        scrollVel -= scrollDeccel;
-
-        scroll = scroll % 600
-
-
-        document.getElementById("Operator").style.setProperty("--Mod", (scroll < -300 ? 1 : 0))
-
-        setScroll(scroll)
-
-        window.requestAnimationFrame(rolling);
-
-    } else {
-        console.log("Scroll Complete")
-        window.requestAnimationFrame(center);
-    }
-}
-
-function center() {
-    if (Math.abs(scroll % 300) > 0) {
-        if (Math.abs(scroll % 300) < Math.abs(scrollVel/10)) {
-            scrollVel = 0
-            result()
-        } else {
-            scroll -= scrollVel/10
-            setScroll(scroll)
-            window.requestAnimationFrame(center);
-        }
-    } else {
-        scrollVel = 0
-        result()
-    }
-}
-
-var currentAudio = null;
-
-function result() {
-    let opName = Op1
-    if (scroll > -450) {
-        opName = Op2
-    }
-    if (currentlyDefending) {
-        currentAudio = new Audio(`Sounds/Defenders/${opName}.wav`);
-        currentAudio.volume = 0.5;
-        currentAudio.play();
-    } else {
-        console.log(`Sounds/Attackers/${opName}.wav`)
-        currentAudio = new Audio(`Sounds/Attackers/${opName}.wav`);
-        currentAudio.volume = 0.5;
-        currentAudio.play();
-    }
-    
+function toggleBan(map) {
+    map.mainDiv.classList.toggle("banned");
+    var audio = new Audio("Sounds/BanMap.wav");
+    audio.volume = 0.2;
+    audio.play()
 }
